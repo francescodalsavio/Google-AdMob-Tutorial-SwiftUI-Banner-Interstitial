59aa598ab06daaafdfac97622a6a0a6311a494ed
Francesco Dal Savio <dalsaviof@gmail.com>
2021-03-14 11:56:05 +0100

    Initial Commit

---
+++ Google AdMob Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Google AdMob Tutorial
+//
+//  Created by Francesco Dal Savio on 14/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Google AdMob Tutorial/Google_AdMob_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Google_AdMob_TutorialApp.swift
+//  Google AdMob Tutorial
+//
+//  Created by Francesco Dal Savio on 14/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Google_AdMob_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
